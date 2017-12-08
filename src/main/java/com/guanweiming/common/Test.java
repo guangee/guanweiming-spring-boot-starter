be8bce77a9006c27da83f8ec133518e130c26861
zziaguan <zziaguan@163.com>
2017-12-08 17:00:29 +0800

    概率工具

--- src/main/java/com/guanweiming/common/Test.java
+++ src/main/java/com/guanweiming/common/Test.java
@@ -1,14 +1,31 @@
 package com.guanweiming.common;
 
-import com.guanweiming.common.util.ip.JuHe;
-import org.json.JSONObject;
+import com.google.common.collect.Maps;
+import com.guanweiming.common.util.RateUtil;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author https://github.com/zziaguan/
  */
 public class Test {
-    public static void main(String[] args){
-        JSONObject object = new JuHe().getResult("125.34.220.179");
-        System.out.println(object);
+    public static void main(String[] args) throws Exception {
+        RateUtil<String> util = new RateUtil<String>();
+        List<RateUtil.RateItem<String>> list=new LinkedList<RateUtil.RateItem<String>>();
+        list.add(new RateUtil.RateItem<String>(1,"苹果"));
+        list.add(new RateUtil.RateItem<String>(2,"橘子"));
+        list.add(new RateUtil.RateItem<String>(3,"香蕉"));
+        Map<String,Integer> map = Maps.newHashMap();
+        for (int i = 0; i < 1000000; i++) {
+            String result = util.getRate(list);
+            if(map.get(result)!=null){
+                map.put(result,map.get(result)+1);
+            }else {
+                map.put(result,1);
+            }
+        }
+        System.out.println(map);
     }
 }
